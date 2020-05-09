5191a701c13d5dbb1d740cd82007665ffa4d919e
Tharindu Bandara <37532030+Tharindu961@users.noreply.github.com>
2020-05-09 17:38:24 +0530

    added more libraries

--- Send_Email/src/send_email/Send_Email.java
+++ Send_Email/src/send_email/Send_Email.java
@@ -5,6 +5,17 @@
  */
 package send_email;
 
+
+import java.net.Authenticator;
+import java.net.PasswordAuthentication;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.mail.Message;
+import javax.mail.Session;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
 /**
  *
  * @author ASUS
@@ -12,7 +23,36 @@ package send_email;
 public class Send_Email {
 
     public static void sendMail(String recepient) {
+        Properties properties = new Properties();
         
+        properties.put("mail.smtp.auth","true");
+        properties.put("mail.smtp.starttls.enable","true");
+        properties.put("mail.smtp.host","smtp.gmail.com");
+        properties.put("mail.smtp.port","587"); 
+        
+        String myAccountEmail = "xxxx@gmail.com";
+        String password = "xxxxxx";
+        
+        Session session = Session.getInstance(properties, new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(myAccountEmail, password);
+            }
+        });
+        
+        Message message = prepareMessage(sessoin, myAccountEmail, recepient);
+        
+    }
+    private static Message prepareMessage(Session session, String myAccountEmail, String recepient){
+        try {
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(myAccountEmail));
+            message.setRecipient(Message.RecipientType.TO, new InternetAddress(recepient));
+            message.setText(recepient);
+        }catch (Exception ex) {
+            Logger.getLogger(Send_Email.class.getName()).log(Level.SEVERE,null,ex);
+        }
+        return null;
     }
     
 }
